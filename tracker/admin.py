627492397381cd7f7624d5af32fc3eaa17756032
Vismaya_Koorma <vismayak813@gmail.com>
2026-05-02 20:46:54 +0530

    Register UserProfile to admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Task, Goal, Timetable, Reminder
+from .models import Task, Goal, Timetable, Reminder, UserProfile
 
 admin.site.register(Task)
 admin.site.register(Goal)
 admin.site.register(Timetable)
 admin.site.register(Reminder)
+admin.site.register(UserProfile)
