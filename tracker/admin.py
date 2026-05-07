c6c28250bdad15d301b345bf7e55346c133379f3
Vismaya_Koorma <vismayak813@gmail.com>
2026-05-07 10:12:15 +0530

    Upgrade Django to 6.0.3 for Python 3.14 compatibility

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Task, Goal, Timetable, Reminder, UserProfile
+from .models import Task, Goal, Timetable, Reminder, UserProfile, StudentTeacherLink
 
 admin.site.register(Task)
 admin.site.register(Goal)
 admin.site.register(Timetable)
 admin.site.register(Reminder)
 admin.site.register(UserProfile)
+admin.site.register(StudentTeacherLink)
